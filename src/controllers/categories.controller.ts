fc03eee18e73ccb1507d2d634aecaefad2d4bf5f
yoelvp <yoelvp73@gmail.com>
2024-04-20 13:24:08 -0500

    feat: add initial services for manage categories

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,9 +1,41 @@
 import { Request, Response } from "express";
+import { throwError } from "@/utils/error.handlers";
+import { CategoriesService } from "@/services/categories.service";
+import { Category } from "@/models/category.model";
+
+const categoriesService = new CategoriesService();
 
 export class CategoriesController {
-  async getCategories(_: Request, res: Response) {
-    return res.json({
-      message: 'world'
-    })
+  public async getCategoryById(req: Request, res: Response) {
+    const params = req.params;
+
+    try {
+      const category = await categoriesService.getCategoryById({ categoryId: params?.id });
+      res.json(category).status(200);
+    } catch (error: any) {
+      throwError(res, error);
+    }
+  }
+
+  public async getCategories(_: Request, res: Response) {
+    try {
+      res.json({
+        data: await categoriesService.getCategories()
+      });
+    } catch (error: any) {
+      throwError(res, error);
+    }
+  }
+
+  public async createCategory(req: Request, res: Response) {
+    const data: Category = req.body;
+
+    try {
+      res.json({
+        data: await categoriesService.createCategory({ data })
+      }).status(201);
+    } catch (error: any) {
+      throwError(res, error);
+    }
   }
 }
