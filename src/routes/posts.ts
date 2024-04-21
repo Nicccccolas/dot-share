fa508369fc1d4d14c0cd951da8fb779e78827195
yoelvp <yoelvp73@gmail.com>
2024-04-20 22:48:44 -0500

    feat: add router for post

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,5 +1,11 @@
+import { PostsController } from "@/controllers/posts.controller";
 import { Router } from "express";
 
 const router = Router();
+const postsController = new PostsController();
+
+router.get("/", postsController.getAll);
+router.get("/:id", postsController.getById);
+router.post("/", postsController.create);
 
 export { router };
